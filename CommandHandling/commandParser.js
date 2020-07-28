98e436f359d882807feed0cca86afc742f39b1f5
Brennan Grieve <46986566+brennanGrieve@users.noreply.github.com>
2020-07-28 14:49:17 +1200

    Initial Commit
    This feature was developed offline and was not pushed to the repository in real time. This is (currently) finished implementation of the tag system.

--- CommandHandling/commandParser.js
+++ CommandHandling/commandParser.js
@@ -1,5 +1,8 @@
+const cardDrawHandlers = require('./cardDrawer/drawingCommandHandlers.js');
 const searchHandlers = require('./searchHandlers.js');
 const texthandlers = require('./textCommandHandlers.js');
+const imageHandlers = require('./imageCommandHandlers.js');
+const tagHandlers = require('./tagCommandHandlers.js');
 
 
 module.exports = {
@@ -12,9 +15,10 @@ module.exports = {
  * @param {Discord.message} message - An object representing the message being evaluated.
  * @param {string} command - The extracted command from the message to be checked.
  * @param {string[]} args  - The parameters added to the message.
+ * @param {Discord.Client} client - Some functions require a reference to the client for specific interactions with the Discord api.
  */
 
-async function parseCommand(message, command, args){
+async function parseCommand(message, command, args, client){
 	valid = 0;
 
 	/**
@@ -37,22 +41,66 @@ async function parseCommand(message, command, args){
 	}
 
 	/** 
-	 * Text based command handlers. These commands do not use any resources on disk, or make any API calls. 
+	 * Text based command handlers. These commands do not use any resources on disk, or make any API calls. Generally speaking, they're simple. 
+	 * 
 	 * avatar - Returns either the sender's avatar (No parameters) or the specified user's avatar.
+	 * thump - Sends the bot after the specified user.
 	 * help - Has the bot DM a list of its commands.
+	 * poke - Sends the bot to poke the specified user. (Functionally the same as thump, but with a different message.)
 	 */
 
 	if(command == "avatar"){ 
 		valid = 1;
 		texthandlers.avatarHandler(message);
 	}
-	
+
+		
     if(command == "help"){
     	valid = 1;
     	texthandlers.helpHandler(message);
 	}
-	
-    return valid;
-    
+
+	/**
+	 * Tag System Command handlers.
+	 * t acts as an additional prefix that directs inputted commands to this handler.
+	 * All CRUD operations are available - 
+	 * - Add (Create)
+	 * - Read (Via tag name as param)
+	 * - Edit (Update),
+	 * - Remove (Delete). 
+	 * Also supports listing of all tags assigned to a single user, and checking to find the owner 
+	 * of any given tag. 
+	 * Passes along the invoking message, and any relevant args to the relevant tag command handler.
+	 */
+
+	if(command == "t"){
+		valid = 1;
+		if(message.attachments.first() != null){
+			args[args.length] = message.attachments.first().url;
+		}
+		if(args[0] == null){
+			message.channel.send("Empty command.");
+			return valid;
+		}
+		if(args[0].toLowerCase() == 'add' || args[0].toLowerCase() == 'create'){
+			tagHandlers.addTag(args, message);
+		}else if(args[0].toLowerCase() == 'remove' || args[0].toLowerCase() == 'delete'){
+			tagHandlers.removeTag(args[1], message);
+		}else if(args[0].toLowerCase() == 'owner'){
+			tagHandlers.getOwner(args[1], message, client)
+		}else if(args[0].toLowerCase() == 'edit'){
+			tagHandlers.editTag(args, message)
+		}else if(args[0].toLowerCase() == 'list'){
+			if(message.mentions.users.first() != null){
+				target = await client.users.fetch(message.mentions.users.first().id);
+				tagHandlers.listUserTags(message, target, client)
+			}else{
+				tagHandlers.listUserTags(message, null, client)
+			}
+		}else{
+			tagHandlers.searchTag(args[0], message.channel);
+		}
+	}
+	return valid;
 }
 
